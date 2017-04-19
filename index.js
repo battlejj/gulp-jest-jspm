a8adfd360995b0a5b957f0b8aeb089be1453e55d
Jeremy Battle <battlejj@gmail.com>
2017-04-18 21:15:21 -0400

    fixed exports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const lib = require("./lib/index");
-module.exports = lib.jest;
+module.exports = lib.default;
 module.exports.makeJestConfig = lib.makeJestConfig;
 module.exports.getJestConfig = lib.makeJestConfig; //alias
